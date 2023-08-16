2dd02dff5d27a313b2d68ed36d74343916e5813d
shabinaf <shabeena.farveen@rootcodelabs.com>
2023-08-16 15:12:18 +0530

    initial commit

---
+++ src/main/java/rootcodelabs/slackintegration/SlackIntegrationApplication.java
@@ -0,0 +1,13 @@
+package rootcodelabs.slackintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rootcodelabs/slackintegration/SlackIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package rootcodelabs.slackintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
